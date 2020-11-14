e51e48f8569dd8f5fc1ede4db1e9e24a5c427412
Tejeshwar Amirthy <tejuamirthi@gmail.com>
2020-11-14 23:29:09 +0530

    Add files via upload

---
+++ DraggableUIView/DraggableUIView.h
@@ -0,0 +1,19 @@
+//
+//  DraggableUIView.h
+//  DraggableUIView
+//
+//  Created by Amirthy Tejeshwar on 14/11/20.
+//  Copyright © 2020 Amirthy Tejeshwar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DraggableUIView.
+FOUNDATION_EXPORT double DraggableUIViewVersionNumber;
+
+//! Project version string for DraggableUIView.
+FOUNDATION_EXPORT const unsigned char DraggableUIViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DraggableUIView/PublicHeader.h>
+
+
