8403381f7547cbd8e72b864d12d974670c81eb0d
tejuamirthi <tejuamirthi@gmail.com>
2020-11-15 00:25:55 +0530

    Mode enum created

---
+++ DraggableUIView/Models/DraggableMode.swift
@@ -0,0 +1,13 @@
+//
+//  DraggableMode.swift
+//  DraggableUIView
+//
+//  Created by Amirthy Tejeshwar on 15/11/20.
+//  Copyright © 2020 Amirthy Tejeshwar. All rights reserved.
+//
+
+
+public enum DraggableMode: Int {
+    case fourCorner
+    case leftRightEdge
+}
