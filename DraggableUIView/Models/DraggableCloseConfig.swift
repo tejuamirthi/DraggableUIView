e7a93a974a96c9fceb590c7ff408471dbafa6ce9
tejuamirthi <tejuamirthi@gmail.com>
2020-11-20 23:04:21 +0530

    Created new model files

---
+++ DraggableUIView/Models/DraggableCloseConfig.swift
@@ -0,0 +1,9 @@
+//
+//  DraggableCloseConfig.swift
+//  DraggableUIView
+//
+//  Created by Teju on 20/11/20.
+//  Copyright © 2020 Amirthy Tejeshwar. All rights reserved.
+//
+
+import Foundation
---
+++ DraggableUIView/Models/DraggableViewConfig.swift
@@ -0,0 +1,9 @@
+//
+//  DraggableViewConfig.swift
+//  DraggableUIView
+//
+//  Created by Teju on 20/11/20.
+//  Copyright © 2020 Amirthy Tejeshwar. All rights reserved.
+//
+
+import Foundation
